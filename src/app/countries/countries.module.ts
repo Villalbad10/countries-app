9fbf20f7614cff0d33ec5c991a9cd92eea29ba2c
Diego Villalba <diego.villalba01@uptc.edu.co>
2024-03-12 19:08:36 -0500

    input, rutas hijas

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+import { CountriesRountingModule } from './countries-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ByCapitalPageComponent,
+    ByCountryPageComponent,
+    ByRegionPageComponent,
+    CountryPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    CountriesRountingModule,
+    SharedModule
   ]
 })
 export class CountriesModule { }
